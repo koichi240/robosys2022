d248d19437aa54baf96f5defc480a23d1c99325e
koichi <s19c1089bg@s.chibakoudai.com>
2022-10-27 12:40:46 +0900

    Update

---
+++ test.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SPDX-FileCopyrightText: 2022 
+#SPDX-License-Identfilter: BSD-3-Clause
+
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+### I/O TEST ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+
+exit $res
+
+
+
